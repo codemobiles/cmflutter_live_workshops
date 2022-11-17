36b6400ab1df281d75def4347287142ed5c0226b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-17 20:44:53 +0700

    1234

--- cmflutter0/lib/main.dart
+++ cmflutter0/lib/main.dart
@@ -11,7 +11,26 @@ class CMApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "CMApp",
-      home: Text("CodeMobiles"),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Text("codemobiles.com"),
     );
   }
 }
