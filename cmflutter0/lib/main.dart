6c91505131c92c6a453af7b4392e05f7383d7d00
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-17 20:37:13 +0700

    1234

--- cmflutter0/lib/main.dart
+++ cmflutter0/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CMApp());
 }
 
+class CMApp extends StatelessWidget {
+  const CMApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "CMApp",
+      home: Text("CodeMobiles"),
+    );
+  }
+}
