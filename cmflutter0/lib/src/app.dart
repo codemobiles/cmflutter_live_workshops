ef738162bb619534b8964492c4fd765be072402c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-26 14:33:33 +0700

    1234

--- cmflutter0/lib/src/app.dart
+++ cmflutter0/lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:cmflutter0/src/pages/home/home_page.dart';
 import 'package:cmflutter0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,7 @@ class CMApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "CMApp",
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
