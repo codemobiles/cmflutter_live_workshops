40c0dc496214196b97b0315900f3a0897b118c4c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-02 21:42:09 +0700

    1234

--- cmflutter0/lib/src/app.dart
+++ cmflutter0/lib/src/app.dart
@@ -1,17 +1,23 @@
+import 'package:cmflutter0/src/bloc/login/login_bloc.dart';
 import 'package:cmflutter0/src/pages/home/home_page.dart';
 import 'package:cmflutter0/src/pages/login/login_page.dart';
 import 'package:cmflutter0/src/pages/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CMApp extends StatelessWidget {
   const CMApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "CMApp",
-      routes: AppRoute.all,
-      home: LoginPage(),
-    );
+    final loginBloc = BlocProvider(create: (context) => LoginBloc());
+
+    return MultiBlocProvider(
+        providers: [loginBloc],
+        child: MaterialApp(
+          title: "CMApp",
+          routes: AppRoute.all,
+          home: LoginPage(),
+        ));
   }
 }
