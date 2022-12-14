8c639b76bc64cf3fc079b95958664467ce9ed17a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-14 20:43:32 +0700

    1234

--- cmflutter0/lib/src/services/webapi_service.dart
+++ cmflutter0/lib/src/services/webapi_service.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+
+import 'package:cmflutter0/src/models/youtube_response.dart';
 import 'package:dio/dio.dart';
 
 class WebApiService {
-  Future<void> feed() async {
+  Future<List<Youtube>> feed() async {
     final dio = Dio();
     final result = await dio.get(
         "https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs");
 
-    print(result.data);
+    final youtubeResponse = youtubeResponseFromJson(result.data);
+    print(youtubeResponse.youtubes[3].title);
+    return youtubeResponse.youtubes;
   }
 }
